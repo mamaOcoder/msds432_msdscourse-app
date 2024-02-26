69f514425b689cfcd7b9c4831db04b0195fc52cd
Leslie <lesliekanderson@fastmail.com>
2024-02-26 14:58:50 -0500

    update startup-script

--- startup-script.sh
+++ startup-script.sh
@@ -2,10 +2,10 @@
 # Install Docker
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common gnupg2
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install -y docker-ce
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo apt-get install unzip
 
 # Install Docker Compose
